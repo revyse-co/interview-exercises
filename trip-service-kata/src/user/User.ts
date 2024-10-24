04122e7adbac1f00a16ff56b0c040d93681dfd6b
Jonathan Samples <jonnysamps@gmail.com>
2024-10-24 06:58:08 -1000

    Add trip-service kata

---
+++ trip-service-kata/src/user/User.ts
@@ -0,0 +1,28 @@
+import Trip from "../trip/Trip";
+
+export default class User {
+  private trips: Trip[] = [];
+  private friends: User[] = [];
+
+  constructor(private name: string) {}
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getFriends(): User[] {
+    return this.friends;
+  }
+
+  public addFriend(user: User): void {
+    this.friends.push(user);
+  }
+
+  public addTrip(trip: Trip): void {
+    this.trips.push(trip);
+  }
+
+  public getTrips(): Trip[] {
+    return this.trips;
+  }
+}
